6ad607842dd9254829dace36bb9b484939f1ba45
fleurblanckaert <60987478+fleurblanckaert@users.noreply.github.com>
2020-11-11 23:25:53 -0500

    Included basic input and output

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -1,8 +1,23 @@
+/*
+Author: Fleur Blanckaert
+Date: November 11, 2020
+ */
+
+
+
 package ca.durhamcollege;
 
-public class Main {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        String name = null;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        name = keyboard.nextLine();
+        System.out.printf("You entered: %s",name);
     }
 }
