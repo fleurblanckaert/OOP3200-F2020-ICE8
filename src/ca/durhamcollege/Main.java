3535745622ee4c340a38ca6c794f116ff7d03306
fleurblanckaert <60987478+fleurblanckaert@users.noreply.github.com>
2020-11-12 00:27:10 -0500

    Added multidimensional array

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -55,6 +55,27 @@ public class Main {
             //OUTPUT
             System.out.printf("You entered: %s \n", name);
             System.out.printf("You entered: %.2f \n", age);
+
+        //MULTI-DIMENSIONAL ARRAY
+        System.out.println("Multi-Dimensional Array example");
+        int rows = 8;
+        int cols = 8;
+        float[][] board = new float[rows][cols];
+
+        for (int x = 0; x < rows; x++) {
+            for (int y = 0; y < cols; y++) {
+                board[x][y] = (float)(Math.random() * 100.0) + 1.0f;
+            }
         }
+
+        System.out.println("Showing 10 Random Values: ");
+        for (int i = 0; i < 10; i++) {
+            var randRows = (int)(Math.random() * rows);
+            var randCols = (int)(Math.random() * cols);
+            System.out.print("For row: " + randRows + " col: " + randCols + " ");
+            System.out.printf("The Random number is: %.2f \n", board[randRows][randCols]);
+        }
+
+    }
     }
 
